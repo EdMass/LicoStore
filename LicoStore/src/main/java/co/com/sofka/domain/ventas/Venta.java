f9dc5b86549114ec8f7034e2e5dc03ae15f17112
EdMass <zairmass@gmail.com>
2022-03-10 15:20:10 -0500

    refactor name ventas to venta

---
+++ LicoStore/src/main/java/co/com/sofka/domain/ventas/Venta.java
@@ -0,0 +1,9 @@
+package co.com.sofka.domain.ventas;
+
+
+import co.com.sofka.domain.annotation.Aggregate;
+import co.com.sofka.domain.ventas.value.VentaID;
+
+public class Venta  {
+
+}
--- LicoStore/src/main/java/co/com/sofka/domain/ventas/Ventas.java
+++
@@ -1,6 +0,0 @@
-package co.com.sofka.domain.ventas;
-
-
-public class Ventas  {
-
-}
