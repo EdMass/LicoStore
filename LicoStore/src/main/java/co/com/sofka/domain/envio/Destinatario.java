4ed78a525298730fce14dda6daf0296ab64856b0
Gochi <mevere66@gmail.com>
2022-03-11 16:22:15 -0500

    Correcion en entidad destinatario

--- LicoStore/src/main/java/co/com/sofka/domain/envio/Destinatario.java
+++ LicoStore/src/main/java/co/com/sofka/domain/envio/Destinatario.java
@@ -1,4 +1,5 @@
 package co.com.sofka.domain.envio;
+
 import co.com.sofka.domain.envio.value.Telefono;
 import co.com.sofka.domain.envio.value.Nombre;
 import co.com.sofka.domain.envio.value.Direccion;
@@ -8,32 +9,24 @@ import co.com.sofka.domain.ventas.value.ClienteID;
 public class Destinatario extends Cliente {
 
     protected Direccion direccion;
-    protected ClienteID clienteID;
     protected Nombre nombre;
     protected Telefono telefono;
 
-    public Destinatario (ClienteID clienteID,Direccion direccion, Nombre nombre, Telefono telefono) {
+    public Destinatario(ClienteID clienteID, Direccion direccion, Nombre nombre, Telefono telefono) {
 
         super(clienteID);
-        this.clienteID= clienteID;
-        this.direccion= direccion;
-        this.nombre=nombre;
-        this.telefono= telefono;
+        this.direccion = direccion;
+        this.nombre = nombre;
+        this.telefono = telefono;
 
     }
 
-    public Direccion getDireccion() {
-        return direccion;
-    }
-
-    public ClienteID getClienteID() {
-        return clienteID;
-    }
 
     public Direccion direccion() {
         return direccion;
     }
-    public void agregarDireccionACliente(ClienteID clienteID, Nombre nombre){
+
+    public void agregarDireccionACliente(ClienteID clienteID, Nombre nombre) {
 
     }
 
